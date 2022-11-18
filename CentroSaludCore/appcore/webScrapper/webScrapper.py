7ea2e61518ff5401d36e4a8de5c43792e0a1cf88
Miguel Salvador <mmsalrub@inf.upv.es>
2022-11-19 00:57:37 +0100

    Implemento que el navegador pregunte por la localización

--- CentroSaludCore/appcore/webScrapper/webScrapper.py
+++ CentroSaludCore/appcore/webScrapper/webScrapper.py
@@ -2,26 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-option = Options()
-
-option.add_argument("--disable-infobars")
-option.add_argument("start-maximized")
-option.add_argument("--disable-extensions")
-
-# Pass the argument 1 to allow and 2 to block
-option.add_argument(
-    ("prefs", {"profile.default_content_setting_values.notifications": 1})
-)
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+seleniumOptions = Options()
+# Evita que el navegador pregunte por la localización
+seleniumOptions.set_preference("geo.enabled", False)
 driver = webdriver.Firefox(
-    options=option
+    options=seleniumOptions
 )
 driver.get("https://www.coordenadas-gps.com/")
-# waiting = WebDriverWait(driver, 10);
-# driver.switch_to.alert()
-elem = driver.find_element(By.ID, "address")
-elem.clear()
-elem.send_keys("Carrer Bisbe Orbe, Alcoy")
-elem.send_keys(Keys.RETURN)
+adContent = driver.find_element(By.ID, "ad-content")
+driver.execute_script("arguments[0].scrollIntoView();", adContent)
+time.sleep(3)
+addressInput = driver.find_element(By.ID, "address")
+addressInput.clear()
+addressInput.send_keys("Carrer Bisbe Orbe, Alcoy")
+addressInput.send_keys(Keys.RETURN)
+getCoordinatesButton = driver.find_element(By.XPATH, '//button[text()="Obtener Coordenadas GPS"]')
+getCoordinatesButton.click()
+
+
+
+
+
+# getCoordinatesButton = driver.find_element(By.XPATH, '//button[text()="Obtener Coordenadas GPS"]')
+# getCoordinatesButton.click()
 # assert "No results found." not in driver.page_source
 # driver.close()
