4c5ab8028ec88d2ec807ae57c30ed3b364a30268
Miguel Salvador <mmsalrub@inf.upv.es>
2022-11-19 11:08:01 +0100

    Obtengo latitud y longitud

--- CentroSaludCore/appcore/webScrapper/webScrapper.py
+++ CentroSaludCore/appcore/webScrapper/webScrapper.py
@@ -17,10 +17,20 @@ driver.execute_script("arguments[0].scrollIntoView();", adContent)
 time.sleep(3)
 addressInput = driver.find_element(By.ID, "address")
 addressInput.clear()
-addressInput.send_keys("Carrer Bisbe Orbe, Alcoy")
+addressInput.send_keys("Godella")
 addressInput.send_keys(Keys.RETURN)
 getCoordinatesButton = driver.find_element(By.XPATH, '//button[text()="Obtener Coordenadas GPS"]')
 getCoordinatesButton.click()
+
+latitudeInput = driver.find_element(By.ID, "latitude")
+longitudeInput = driver.find_element(By.ID, "longitude")
+latitude = latitudeInput.get_attribute('value')
+longitude = longitudeInput.get_attribute('value')
+while (latitude == latitudeInput.get_attribute('value') or longitude == longitudeInput.get_attribute('value')):
+    time.sleep(0.2)
+latitude = latitudeInput.get_attribute('value')
+longitude = longitudeInput.get_attribute('value')
+print('la latitud de Godella es ' + latitude + ' y la longitud es ' + longitude)
 
 
 
