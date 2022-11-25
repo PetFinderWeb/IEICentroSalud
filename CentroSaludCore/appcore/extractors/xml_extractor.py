740ba94d060e395c3df4575829254e91d6c1a900
Joel L. Ruíz Carrillo <luruicar@upv.edu.es>
2022-11-25 18:35:13 +0100

    import de webscrapper en xml

--- CentroSaludCore/appcore/extractors/xml_extractor.py
+++ CentroSaludCore/appcore/extractors/xml_extractor.py
@@ -9,6 +9,7 @@ from appcore.webScrapper.webScrapper import WebScrapper
 
 
 class XML_Extactor(Extractor):
+    webScrapper: WebScrapper
 
     def abrir_fichero(self, path: str = os.path.join(MEDIA_ROOT, 'biblioteques.xml')) -> IO:
         return open(path, mode='r', encoding='utf-8')
@@ -19,56 +20,48 @@ class XML_Extactor(Extractor):
         json_object = json.loads(xmlJson)
         return json_object['response']['row']['row']
 
-    
     def map_codigo_provincia(self, centro: Dict[str, Any]) -> str:
         return "07"
 
-    
     def map_nombre_provincia(self, centro: Dict[str, Any]) -> str:
         return "Islas baleares"
 
-    
     def map_codigo_localidad(self, centro: Dict[str, Any]) -> str:
-        return "autoincremental"
-    
-    
+        return ""
+
     def map_nombre_localidad(self, centro: Dict[str, Any]) -> str:
-        return centro["municipi"] 
-    
-    
+        return centro["municipi"]
+
     def map_nombre_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
         return centro["nom"]
-    
 
     def map_tipo_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
         tipoCentro = centro["funcio"]
-        if tipoCentro == "CENTRE SANITARI" or tipoCentro == "UNITAT BÀSICA" :
+        if tipoCentro == "CENTRE SANITARI" or tipoCentro == "UNITAT BÀSICA":
             res = "CENTRO DE SALUD"
         else:
             res = "OTROS"
         return res
 
-
     def map_direccion_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
         return centro["adreca"]
 
-    
     def map_codigopostal_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
-        self.latlangcode = self.webScrapper.searchByCoordinates(self,centro["lat"] ,centro["long"] )
+        self.latlangcode = self.webScrapper.searchByCoordinates(
+            self, centro["lat"], centro["long"])
+        if (self.latlangcode == None):
+            return None
+        else:
+            return self.latlangcode
 
-    
     def map_longitud_establecimiento_sanitario(self, centro: Dict[str, Any]) -> float:
         return centro["long"]
 
-    
     def map_latitud_establecimiento_sanitario(self, centro: Dict[str, Any]) -> float:
         return centro["lat"]
 
-    
     def map_telefono_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
         return None
 
-    
     def map_descripcion_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
         return None
-
