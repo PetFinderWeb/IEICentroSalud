78cf92cd32e8da3d9e52d23deefd65078ae3c17d
Joel L. Ruíz Carrillo <luruicar@upv.edu.es>
2022-11-25 18:20:34 +0100

    5 digitos codigo postal

--- CentroSaludCore/appcore/extractors/xml_extractor.py
+++ CentroSaludCore/appcore/extractors/xml_extractor.py
@@ -1,4 +1,3 @@
-
 import os
 from typing import *
 from CentroSaludCore.settings import MEDIA_ROOT
@@ -6,17 +5,70 @@ import xml.etree.ElementTree as ET
 import xmltodict
 from appcore.extractors.extractor import Extractor
 import json
+from appcore.webScrapper.webScrapper import WebScrapper
 
 
 class XML_Extactor(Extractor):
 
-    def abrir_fichero(self) -> IO:
-        path = os.path.join(MEDIA_ROOT, 'biblioteques.xml')
-        return open(path, mode='r')
+    def abrir_fichero(self, path: str = os.path.join(MEDIA_ROOT, 'biblioteques.xml')) -> IO:
+        return open(path, mode='r', encoding='utf-8')
 
     def analizar_datos(self, file: IO) -> List[Dict[str, Any]]:
-        xmldict = xmltodict.parse(file)
-        xmlJson = json.dumps(xmldict)   
+        xmldict = xmltodict.parse(file.read())
+        xmlJson = json.dumps(xmldict, ensure_ascii=False)
+        json_object = json.loads(xmlJson)
+        return json_object['response']['row']['row']
+
+    
+    def map_codigo_provincia(self, centro: Dict[str, Any]) -> str:
+        return "07"
+
+    
+    def map_nombre_provincia(self, centro: Dict[str, Any]) -> str:
+        return "Islas baleares"
+
+    
+    def map_codigo_localidad(self, centro: Dict[str, Any]) -> str:
+        return "autoincremental"
+    
+    
+    def map_nombre_localidad(self, centro: Dict[str, Any]) -> str:
+        return centro["municipi"] 
+    
+    
+    def map_nombre_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
+        return centro["nom"]
+    
+
+    def map_tipo_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
+        tipoCentro = centro["funcio"]
+        if tipoCentro == "CENTRE SANITARI" or tipoCentro == "UNITAT BÀSICA" :
+            res = "CENTRO DE SALUD"
+        else:
+            res = "OTROS"
+        return res
 
 
+    def map_direccion_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
+        return centro["adreca"]
+
+    
+    def map_codigopostal_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
+        self.latlangcode = self.webScrapper.searchByCoordinates(self,centro["lat"] ,centro["long"] )
+
+    
+    def map_longitud_establecimiento_sanitario(self, centro: Dict[str, Any]) -> float:
+        return centro["long"]
+
+    
+    def map_latitud_establecimiento_sanitario(self, centro: Dict[str, Any]) -> float:
+        return centro["lat"]
+
+    
+    def map_telefono_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
+        return None
+
+    
+    def map_descripcion_establecimiento_sanitario(self, centro: Dict[str, Any]) -> str:
+        return None
 
