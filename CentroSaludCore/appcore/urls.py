dbabac5d140b4dc093cec18b25313359100c97a0
renatillas <renata.amutio@gmail.com>
2022-12-20 17:34:49 +0100

    De todo un poco

--- CentroSaludCore/appcore/urls.py
+++ CentroSaludCore/appcore/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('cargar_csv/', views.cargar_csv, name='csv'),
     path('cargar_xml/', views.cargar_xml, name='xml'),
     path('cargar_json/', views.cargar_json, name='json'),
-    path('borrar_bdd/', views.borrar_bdd, name='borrar_bdd')
+    path('borrar_bdd/', views.borrar_bdd, name='borrar_bdd'),
+    path('carga_parametrizada', views.carga_parametrizada, name='carga_parametrizada'),
+    path('busqueda', views.busqueda, name='busqueda'),
 ]
--- CentroSaludCore/appcore/views.py
+++ CentroSaludCore/appcore/views.py
@@ -27,3 +27,41 @@ def borrar_bdd(request):
     Provincia.objects.all().delete()
     Localidad.objects.all().delete()
     return HttpResponse(status=200)
+
+def carga_parametrizada(request):
+    if request.GET.get('p', False) == 'true':
+        extractor = JSON_Extractor()
+        extractor.extraer_de_fichero()
+    if request.GET.get('c', False) == 'true':
+        extractor = CSV_Extractor()
+        extractor.extraer_de_fichero()
+    if request.GET.get('b', False) == 'true':
+        extractor = XML_Extactor()
+        extractor.extraer_de_fichero()
+    
+    return HttpResponse(status=200)
+
+def busqueda(request):
+    localidad = request.GET.get('localidad', '')
+    cod_postal = request.GET.get('cod_postal', '')
+    provincia = request.GET.get('provincia', '')
+    tipo = request.GET.get('tipo', 'T')
+    query_set = Establecimiento_Sanitario.objects.all()
+    query_provincia = Establecimiento_Sanitario.objects.none()    
+    query_localidades = Establecimiento_Sanitario.objects.none()    
+    query_cod_postal = Establecimiento_Sanitario.objects.none()
+    query_tipo = Establecimiento_Sanitario.objects.none()
+
+    if localidad != "":
+        query_localidades = query_set.filter(en_localidad__nombre=localidad)
+    if cod_postal != "":
+        query_cod_postal = query_set.filter(codigo_postal=cod_postal)
+    if provincia != "":
+        query_provincia = query_set.filter(en_localidad__en_provincia__nombre=provincia)
+        print(query_provincia)
+    if tipo != 'T':
+        query_tipo = query_set.filter(tipo=tipo)
+        
+    query_final = query_localidades.union(query_cod_postal).union(query_provincia).intersection(query_tipo)
+    
+    return HttpResponse(status=200)
