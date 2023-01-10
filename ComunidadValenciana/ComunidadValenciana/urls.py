b24880b54b277a54d1ae7841ac975361a341e468
Miguel Salvador <mmsalrub@inf.upv.es>
2023-01-10 11:35:47 +0100

    Refactorización del wrapper de la comunidad valenciana

--- ComunidadValenciana/ComunidadValenciana/urls.py
+++ ComunidadValenciana/ComunidadValenciana/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # el endpoint del wrapper se encuentra definito en wrapper.urls
     path('wrapper/', include('wrapper.urls'))
 ]
